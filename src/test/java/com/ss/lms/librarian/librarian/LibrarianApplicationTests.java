b96d0d79c0441fe62ebf7726879ed3dbefe8fdc6
Saurav <sbhuju61@gmail.com>
2020-02-24 10:59:54 -0700

    initial commit

---
+++ src/test/java/com/ss/lms/librarian/librarian/LibrarianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.lms.librarian.librarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
